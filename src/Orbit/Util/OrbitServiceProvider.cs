d6ce9d0506a1fe9d7bf9e21a45b5c47d8d1dc665
Josh <jdegraw42@gmail.com>
2019-10-16 16:56:11 -0600

    Added OrbitServiceProvider

---
+++ src/Orbit/Util/OrbitServiceProvider.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orbit.Util
+{
+    public class OrbitServiceProvider: IServiceProvider
+    {
+        private readonly IServiceProvider _provider;
+        private OrbitServiceProvider()
+        {
+            this._provider = Build();
+        }
+
+        private static readonly Lazy<OrbitServiceProvider> _instance = new Lazy<OrbitServiceProvider>(() => new OrbitServiceProvider());
+
+        public static IServiceProvider Instance => _instance.Value;
+
+        private IServiceProvider Build()
+        {
+            var services = new ServiceCollection();
+            RegisterServices(services);
+            return services.BuildServiceProvider();
+        }
+
+        private void RegisterServices(IServiceCollection services)
+        {
+            //TODO: Register service implementations
+        }
+
+        object IServiceProvider.GetService(Type serviceType) => _provider.GetService(serviceType);
+
+    }
+}
