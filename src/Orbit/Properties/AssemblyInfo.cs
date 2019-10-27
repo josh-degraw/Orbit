b37918ffc4f2626cb29e2a5e5b0f7445cbebd76a
Josh <jdegraw42@gmail.com>
2019-10-26 21:37:16 -0600

    Added some more boilerplate configurations

---
+++ src/Orbit/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Resources;
+using System.Runtime.InteropServices;
+
+// Setting ComVisible to false makes the types in this assembly not visible to COM components. If you need to access a
+// type in this assembly from COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2cba0531-8146-4e53-8403-29a36f8bc5f9")]
+[assembly: NeutralResourcesLanguage("en-US")]
