c32934b0700201dd75bcc30b1c006968f2f668eb
Candra Ray <candraray@hotmail.com>
2019-11-12 06:54:09 -0700

    Preliminary water system component class; pending subdivision

---
+++ src/Orbit/Models/WaterSystem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orbit.Models
+{
+    class WaterSystem
+    {
+        int urinePretreatmentLevel;
+        int urineTankLevel;
+        bool urineTankToDistillerPumpOn;
+        string diverterValvePosition;
+       
+        bool distillerReady;
+        int distillerMotorSpeed;
+        double distillerVibration;
+        int distillerTemp;
+        bool distillerToBrineFilterPumpOn;
+        int brineTankLevel;
+        
+        bool distillateToGasSeperatorPumpOn;
+        bool distillateGasSeperatorOk;
+        bool distillateToProcessorPumpOn;
+
+        int grayWaterTankLevel;
+        bool grayWaterToFiltersPumpOn;
+        
+        bool filterInflowOk;
+        bool filterOutflowOk;
+
+        int preHeaterTemp;
+        bool catalystReactorOk;
+
+        bool waterQualitySensorOk;
+        string waterReprocessValvePosition;
+        
+        int cleanTankLevel;
+
+        bool systemOnOff;
+        bool systemReady;
+
+
+    }
+}
