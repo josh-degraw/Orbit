92059bd77d54d327fd3bf857807edcca9a847a7b
Candra Ray <candraray@hotmail.com>
2019-11-12 06:57:54 -0700

    Initial Thermal (cabin heating and cooling) system class. Pending subdivision (internal/external/cabin)

---
+++ src/Orbit/Models/ThermalSystem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orbit.Models
+{
+    class ThermalSystem
+    {
+        double actualCabinTemp;
+        double selectedCabinTemp;
+
+        double internalCoolantTemp;
+        bool internalCoolantPumpOn;
+        double internalCoolantPumpTemp;
+        double internalCoolantPumpVibration;
+        int internalCoolantTankLevel;
+        bool cabinCoolantLeakSensorTripped;
+
+        double externalCoolantTemp;
+        bool externalcoolantPumpOn;
+        double externalCoolantPumpTemp;
+        double externalCoolantPumpVibration;
+        int externalCoolantTankLevel;
+
+    }
+}
