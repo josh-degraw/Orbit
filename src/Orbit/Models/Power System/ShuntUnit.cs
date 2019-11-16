7b258ea4d1b9f38385750b8496ec159637a0acb3
Daniel Jones <dannyboy.2010@yahoo.com>
2019-11-16 16:16:00 -0700

    Basic Power System Components and classes.

---
+++ src/Orbit/Models/Power System/ShuntUnit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orbit.Models.Power_System
+{
+    public class ShuntUnit
+    {
+        public double inputVoltage;
+        public double outputVoltage;
+    }
+}
