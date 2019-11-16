a7f0300499a0428538accd9c533aaac44c01bcd3
Daniel Jones <dannyboy.2010@yahoo.com>
2019-11-16 16:16:00 -0700

    Basic Power System Components and classes.

---
+++ src/Orbit/Models/Power System/Radiator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orbit.Models.Power_System
+{
+    public class Radiator
+    {
+        public double Temperature;
+        public bool Deployed;
+    }
+}
---
+++ src/Orbit/Models/Power System/SolarArray.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orbit.Models.Power_System
+{
+    public class SolarArray
+    {
+        
+        public double Voltage;
+
+        //In Kilowatts
+        public double Power;
+
+        public bool Deployed;
+    }
+}
