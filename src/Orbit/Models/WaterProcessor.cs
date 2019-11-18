22f85d91fac18281b5f435d4d5aefbd47870f79a
Candra <Candraray@hotmail.com>
2019-11-18 16:52:25 -0700

    Redesign water processor system properties

---
+++ src/Orbit/Models/WaterProcessor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkTest
+{
+    public class WaterProcessor
+    {
+        public Guid ID { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+        public string SupplyPump { get; set; }
+        public string PostFilterContaminateSensor { get; set; }
+        public int PreHeaterTemp { get; set; }
+        public int CatalyticReactorTemp { get; set; }
+        public string ReactorHealthSensor { get; set; }
+        public string ReprocessDiverterValve { get; set; }
+        public int ProductTankLevel { get; set; }
+        public string DeliveryPump { get; set; }
+       
+        //public virtual ICollection<Alarm> Alarms { get; set; }
+
+    }
+}
