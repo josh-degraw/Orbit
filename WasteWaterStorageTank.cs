aaa5c181b605ff97ab420afd1b09f5730c865292
Candra <Candraray@hotmail.com>
2019-11-18 16:53:20 -0700

    Branch out shared gray water tank
    gray water tank is shared between Urine processor and Water processor systems

---
+++ WasteWaterStorageTank.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkTest
+{
+    public  class WasteWaterStorageTank
+    {
+        public Guid ID { get; set; }
+
+        /// <summary>
+        /// Current level of the wastewater collection tank
+        /// recieves output from urine processing system and other wastewater sources (hygiene...)
+        /// </summary>
+        public int Level { get; set; }
+    }
+}
