bccea31f8b5ea2689c698c017bb78499e01de505
Candra <Candraray@hotmail.com>
2019-11-18 16:55:30 -0700

    Properties for database
    Loop that exists on exterior of station, cools through radiators to space. Radiators rotate in relation to the sun to provide optimal cooling

---
+++ ExternalCoolantLoop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkTest
+{
+    public class ExternalCoolantLoop
+    {
+        public Guid ID { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+        public string PumpStatus { get; set; }
+        public int RadiatorRotation { get; set; }
+        public int Pressure { get; set; }
+        public int OutgoingFluidTemp { get; set; }
+        public int ReturnFluidTemp { get; set; }
+        public int TankLevel { get; set; }
+    }
+}
