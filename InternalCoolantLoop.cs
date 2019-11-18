db718a4ff5667b62ae08c46c30bd941e8382a378
Candra <Candraray@hotmail.com>
2019-11-18 16:56:52 -0700

    Properties for database
    Loop that exists inside the station. Interfaces with various cold plates to cool equipment and air. Circulates water on ISS.

---
+++ InternalCoolantLoop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkTest
+{
+    public class InternalCoolantLoop
+    {
+        public Guid ID { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+        public string PumpStatus { get; set; }
+        public int Pressure { get; set; }
+        public int OutgoingFluidTemp { get; set; }
+        public int ReturnFluidTemp { get; set; }
+        public int ReserveTankLevel { get; set; }
+    }
+}
