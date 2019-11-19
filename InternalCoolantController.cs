5e07f60037bb43e7d745957a4d3cc771facc9682
Candra <Candraray@hotmail.com>
2019-11-18 17:00:50 -0700

    Initial controller/data loader concept
    Idea is emulate the firmware controller of the system by having a class emulate sensor readings, change hardware states (ie pump on/off) and then store that 'momentary' state into the database. The UI would then utilize this state data on the other side of the database.

---
+++ InternalCoolantController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkTest
+{
+    class InternalCoolantController
+    {
+        /// <summary>
+        /// desired cabin temperature, changeable by crew
+        /// </summary>
+        public double CabinSetTemp { get; set; }
+
+        /// <summary>
+        /// current sensor read cabin temperature
+        /// </summary>
+        public double CabinActualTemp { get; private set; }
+        public string PumpOnOff { get; set; }
+
+        /// <summary>
+        /// pressure of coolant in the system
+        /// </summary>
+        public int FluidPressure { get; private set; }
+
+        /// <summary>
+        /// temp of coolant returning from internal/external heat exchanger
+        /// </summary>
+        public int InflowFluidTemp { get; private set; }
+
+        /// <summary>
+        /// temp of coolant from cabin going to internal/external heat exchanger
+        /// </summary>
+        public int OutflowFluidTemp { get; private set; }
+
+        public void GenerateData()
+        {
+
+            Random random = new Random();
+            {
+                double temp = random.Next(210, 289);
+                CabinActualTemp = temp/10 + temp%10;
+                FluidPressure = random.Next(460, 512) / 10;
+                InflowFluidTemp = random.Next(120, 150);
+                OutflowFluidTemp = random.Next(180, 200);
+            }
+
+            IServiceProvider provider = TestServiceProvider.Instance;
+
+            using (var db = provider.GetService<DatabaseContext>())
+            {
+                db.Add(new InternalCoolantLoop()
+                {
+                    ID = new Guid(),
+                    DateTime = DateTimeOffset.Now,
+                    PumpStatus = "On",
+                    Pressure = FluidPressure,
+                    OutgoingFluidTemp = OutflowFluidTemp,
+                    ReturnFluidTemp = InflowFluidTemp,
+                    ReserveTankLevel = 70
+                });
+
+                db.SaveChanges();
+            }
+        }
+    }
+}
