265a705c7d7340062cc66aeff80a2fde2fd1805b
Josh <jdegraw42@gmail.com>
2019-12-08 14:28:37 -0700

    Fixed merge issue

--- src/Orbit/Models/DiverterValvePositions.cs
+++ src/Orbit/Models/DiverterValvePositions.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Orbit.Models
+﻿namespace Orbit.Models
 {
-    public enum  DiverterValvePositions
+    public enum DiverterValvePositions
     {
-        ToWaterProcessor,
-        ToOxygenGenerator
+        ToStorage,
+        Reprocess
     }
 }
