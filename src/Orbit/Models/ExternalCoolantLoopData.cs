acfc65bd976634999d091689c8b6309c7126d26b
Candra <Candraray@hotmail.com>
2019-12-08 14:24:00 -0700

    Thermal control (#11)
    * Properties for database
    
    Loop that exists on exterior of station, cools through radiators to space. Radiators rotate in relation to the sun to provide optimal cooling
    
    * Properties for database
    
    Loop that exists inside the station. Interfaces with various cold plates to cool equipment and air. Circulates water on ISS.
    
    * Initial controller/data loader concept 
    
    Idea is emulate the firmware controller of the system by having a class emulate sensor readings, change hardware states (ie pump on/off) and then store that 'momentary' state into the database. The UI would then utilize this state data on the other side of the database.
    
    * Commented on property/method intentions
    
    * No longer using this class
    
    * new commit on water-system branch
    
    * newer commit on water system branch
    
    * Changed namespace and directory
    
    * changed namespace and directory
    
    * changed namespace and directory
    
    * changed directory for real
    
    * misplaced duplicate
    
    * no longer using this class
    
    * Added property for overall component status
    
    * Added property for overall system status
    
    * added using directives, changed DI provider to OrbitServiceProvider
    
    * Added internal and external cooling systems with value ranges
    
    * Deleted classes no longer in use
    
    * Fixed build problems
    
    * Added tolerances, corrected wrong AlertLevel generation, adjusted methods

---
+++ src/Orbit/Models/ExternalCoolantLoopData.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Orbit.Models
+{
+    public class ExternalCoolantLoopData : IAlertableModel
+    {
+        public DateTimeOffset ReportDateTime { get; set; } = DateTimeOffset.Now;
+
+        /// <summary>
+        /// basically on/off/failure(on by not working)
+        /// </summary>
+        public bool PumpOn { get; set; }
+
+        /// <summary>
+        /// position in degrees from neutral point. 
+        /// </summary>
+        [Range(-215, 215)]
+        public int RadiatorRotation { get; set; }
+
+        [NotMapped]
+        int radiatorRotationUpperLimit = 215;
+        [NotMapped]
+        int radiatorRotationLowerLimit = -215;
+        [NotMapped]
+        int radiatorRotationTolerance = 10;
+
+        /// <summary>
+        /// pressure of fluid in the lines
+        /// </summary>
+        [Range(0, 600)]
+        public int FluidLinePressure { get; set; }
+
+        [NotMapped]
+        int fluidLinePressureUpperLimit = 480;
+        [NotMapped]
+        int fluidLinePressureLowerLimit = 170;
+        [NotMapped]
+        int fluidLinePressureTolerance = 30;
+
+        /// <summary>
+        /// temperature of fluid returning from radiator flow control valve to internal/external heat exchanger
+        /// </summary>
+        [Range(-157, 121)]
+        public double TempFluidToHeatExchanger { get; set; }
+
+        [NotMapped]
+        double tempFluidToHeatExchangerUpperLimit = 18.22;
+        [NotMapped]
+        double tempFluidToHeatExchangerLowerLimit = 1.67;
+        [NotMapped]
+        double tempFluidToHeatExchangerTolerance = 5;
+
+        /// <summary>
+        /// heats the external coolant if the heat offload from the station is too low to warm the -40F degree
+        /// ammonia to at least 36F to prevent the internal coolant (water) from freezing
+        /// </summary>
+        public bool LineHeaterOn { get; set; }
+
+        /// <summary>
+        /// contains repleneshment fluid and serves as additional resevoir for expanded fluid to go (increase of pressure),
+        /// and draw from when fluid contracts (decrease pressure)
+        /// </summary>
+        [Range(0, 100)]
+        public int TankLevel { get; set; }
+
+        int tankLevelUpperLimit = 95;
+        int tankLevelTolerance = 5;
+
+
+        public string ComponentName => "ExternalCoolantSystem";
+
+        private IEnumerable<Alert> CheckRadiatorRotation()
+        {
+            if(RadiatorRotation > radiatorRotationUpperLimit)
+            {
+                yield return new Alert(nameof(RadiatorRotation), "Coolant radiator has exceeded maximum available rotation", AlertLevel.HighError);
+            }
+            else if(RadiatorRotation >= (radiatorRotationUpperLimit - radiatorRotationTolerance))
+            {
+                yield return new Alert(nameof(RadiatorRotation), "Coolant radiator has exceeded allowed rotation", AlertLevel.HighWarning);
+            }
+            else if(RadiatorRotation < radiatorRotationLowerLimit)
+            {
+                yield return new Alert(nameof(RadiatorRotation), "Coolant radiator has exceeded maximum available rotation", AlertLevel.LowError);
+            }
+            else if(RadiatorRotation <= (radiatorRotationLowerLimit + radiatorRotationTolerance))    
+            {
+                yield return new Alert(nameof(RadiatorRotation), "Coolant radiator has exceeded allowed rotation", AlertLevel.LowWarning);
+            }
+            else
+            {
+                yield return Alert.Safe(nameof(RadiatorRotation));
+            }
+        }
+
+        private IEnumerable<Alert> CheckLinePressure()
+        {
+            if (FluidLinePressure >= fluidLinePressureUpperLimit)
+            {
+                yield return new Alert(nameof(FluidLinePressure), "Coolant line pressure is above maximum", AlertLevel.HighError);
+            }
+            else if (FluidLinePressure >= (fluidLinePressureUpperLimit - fluidLinePressureTolerance))
+            {
+                yield return new Alert(nameof(FluidLinePressure), "Coolant line pressure is too high", AlertLevel.HighWarning);
+            }
+            else if (FluidLinePressure < fluidLinePressureLowerLimit)
+            {
+                yield return new Alert(nameof(FluidLinePressure), "Coolant line pressure is below minimum", AlertLevel.LowError);
+            }
+            else if (FluidLinePressure <= (fluidLinePressureLowerLimit + fluidLinePressureTolerance))
+            {
+                yield return new Alert(nameof(FluidLinePressure), "Coolant line pressure is too low", AlertLevel.LowWarning);
+            }
+            else
+            {
+                yield return Alert.Safe(nameof(FluidLinePressure));
+            }
+        }
+
+        private IEnumerable<Alert> CheckFluidTemp()
+        {
+            if(TempFluidToHeatExchanger >= tempFluidToHeatExchangerUpperLimit)
+            {
+                yield return new Alert(nameof(TempFluidToHeatExchanger), "External coolant temperature is above maximum", AlertLevel.HighError);
+            }
+            else if(TempFluidToHeatExchanger >= (tempFluidToHeatExchangerUpperLimit - tempFluidToHeatExchangerTolerance))
+            {
+                yield return new Alert(nameof(TempFluidToHeatExchanger), "External coolant temperature is too high", AlertLevel.HighWarning);
+            }
+            else if (TempFluidToHeatExchanger <= tempFluidToHeatExchangerLowerLimit)
+            {
+                yield return new Alert(nameof(TempFluidToHeatExchanger), "External coolant temperature is below minimum", AlertLevel.LowError);
+            }
+            else if (TempFluidToHeatExchanger <= (tempFluidToHeatExchangerLowerLimit + tempFluidToHeatExchangerTolerance))
+            {
+                yield return new Alert(nameof(TempFluidToHeatExchanger), "External coolant temperature is low", AlertLevel.LowWarning);
+            }
+            else
+            {
+                yield return Alert.Safe(nameof(TempFluidToHeatExchanger));
+            }
+        }
+
+        private IEnumerable<Alert> CheckTankLevel()
+        {
+            if (TankLevel >= tankLevelUpperLimit)
+            {
+                yield return new Alert(nameof(TankLevel), "Tank level at capacity", AlertLevel.HighError);
+            }
+            else if (TankLevel >= (tankLevelUpperLimit - tankLevelTolerance))
+            {
+                yield return new Alert(nameof(TankLevel), "Tank level nearing capacity", AlertLevel.HighWarning);
+            }
+            else
+            {
+                yield return Alert.Safe(nameof(TankLevel));
+            }
+        }
+
+        IEnumerable<Alert> IAlertableModel.GenerateAlerts()
+        {
+            return CheckRadiatorRotation().Concat(CheckLinePressure()).Concat(CheckFluidTemp()).Concat(CheckTankLevel());
+        }
+
+    }
+}                                                                      
---
+++ src/Orbit/Models/InternalCoolantLoopData.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Orbit.Models
+{
+    public class InternalCoolantLoopData : IAlertableModel
+    {
+        public DateTimeOffset ReportDateTime { get; set; } = DateTimeOffset.Now;
+
+        /// <summary>
+        /// basically on/off/failure(on by not working)
+        /// </summary>
+        public bool PumpOn { get; set; }
+
+        /// <summary>
+        /// warmer coolant loop for experiments and avionics
+        /// nominal is 10.5C
+        /// </summary>
+        [Range(0, 47)]
+        public double TempMedCoolantLoop { get; set; }
+
+        [NotMapped]
+        private double medCoolantLoopUpperLimit = 22;
+        [NotMapped]
+        private double medCoolantLoopLowerLimit = 12;
+        [NotMapped]
+        private double medCoolantLoopTolerance = 5;
+
+
+        /// <summary>
+        /// colder coolant loop for life support and, cabin air assembly, and some experiments
+        /// nominal is 4C
+        /// </summary>
+        [Range(0, 47)]
+        public double TempLowCoolantLoop { get; set; }
+
+        [NotMapped]
+        private double lowTempCoolantLoopUpperLimit = 10;
+        [NotMapped]
+        private double lowTempCoolantLoopLowerLimit = 2;
+        [NotMapped]
+        private double lowTempCoolantLoopTolerance = 2;
+
+        [NotMapped]
+        public string ComponentName => "InternalCoolantSystem";
+
+
+        private IEnumerable<Alert> CheckLowTempLoopStatus()
+        {
+            if(TempLowCoolantLoop >= lowTempCoolantLoopUpperLimit)
+            {
+                yield return new Alert(nameof(TempLowCoolantLoop), "Low coolant loop temperature is above maximum", AlertLevel.HighError);
+            }
+            else if (TempLowCoolantLoop >= (lowTempCoolantLoopUpperLimit - lowTempCoolantLoopTolerance))
+            {
+                yield return new Alert(nameof(TempLowCoolantLoop), "Low coolant loop temperature is high", AlertLevel.HighWarning);
+            }
+            else if(TempLowCoolantLoop <= lowTempCoolantLoopLowerLimit)
+            {
+                yield return new Alert(nameof(TempLowCoolantLoop), "Low coolant loop temperature is low", AlertLevel.LowError);
+            }
+            else if(TempLowCoolantLoop <= (lowTempCoolantLoopLowerLimit + lowTempCoolantLoopTolerance))
+            {
+                yield return new Alert(nameof(TempLowCoolantLoop), "Low coolant loop temperature is below minimum", AlertLevel.LowWarning);
+            }
+            else
+            {
+                yield return Alert.Safe(nameof(TempLowCoolantLoop));
+            }
+        }
+
+        private IEnumerable<Alert> CheckMedTempLoopStatus()
+        {
+            if (TempMedCoolantLoop >= medCoolantLoopUpperLimit)
+            {
+                yield return new Alert(nameof(TempMedCoolantLoop), "Med coolant loop temperature is above maximum", AlertLevel.HighError);
+            }
+            else if (TempMedCoolantLoop >= (medCoolantLoopUpperLimit - medCoolantLoopTolerance))
+            {
+                yield return new Alert(nameof(TempMedCoolantLoop), "Med coolant loop temperature is high", AlertLevel.HighWarning);
+            }
+            else if (TempMedCoolantLoop <= medCoolantLoopLowerLimit)
+            {
+                yield return new Alert(nameof(TempMedCoolantLoop), "Med coolant loop temperature is low", AlertLevel.LowError);
+            }
+            else if (TempMedCoolantLoop <= (medCoolantLoopLowerLimit + medCoolantLoopTolerance))
+            {
+                yield return new Alert(nameof(TempMedCoolantLoop), "Med coolant loop temperature is below minimum", AlertLevel.LowWarning);
+            }
+            else
+            {
+                yield return Alert.Safe(nameof(TempMedCoolantLoop));
+            }
+        }
+
+        IEnumerable<Alert> IAlertableModel.GenerateAlerts()
+        {
+            return CheckLowTempLoopStatus().Concat(CheckMedTempLoopStatus());
+        }
+    }
+}
