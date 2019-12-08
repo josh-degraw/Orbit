b7a472150ab73226941b2f80d3bdb84be2d212de
Candra <Candraray@hotmail.com>
2019-12-08 14:19:41 -0700

    Updated classes and added alert generation methods (#13)
    * Updated classes and added alert generation methods
    
    * Added circulation fan speed property and alert generation method
    
    * Moved all  limiting values to variables. Added fanspeed, tolerance values.
    
    * Fixed incorrect alert generation

---
+++ src/Orbit/Models/DiverterValvePositions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orbit.Models
+{
+    public enum  DiverterValvePositions
+    {
+        ToWaterProcessor,
+        ToOxygenGenerator
+    }
+}
---
+++ src/Orbit/Models/SystemStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orbit.Models
+{
+    public enum SystemStatus
+    {
+        On,
+        Ready,
+        Standby,
+        Processing,
+        Trouble
+    }
+}
