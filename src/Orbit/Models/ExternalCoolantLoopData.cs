25c4955fe16073a26577854c69a70d68fe7728f1
Candra Ray <candraray@hotmail.com>
2020-01-11 08:05:28 -0700

    Change properties, added preliminary logic

--- src/Orbit/Models/ExternalCoolantLoopData.cs
+++ src/Orbit/Models/ExternalCoolantLoopData.cs
@@ -7,12 +7,33 @@ namespace Orbit.Models
 {
     public class ExternalCoolantLoopData : IAlertableModel
     {
+        #region Limits
+
+        private const int radiatorRotationUpperLimit = 215;
+        private const int radiatorRotationLowerLimit = -215;
+        private const int radiatorRotationTolerance = 10;
+
+        private const int fluidPressureUpperLimit = 480;
+        private const int fluidPressureLowerLimit = 170;
+        private const int fluidPressureTolerance = 30;
+
+        private const double outputFluidTemperatueUpperLimit = 18.22;
+        private const double outputFluidTemperatureLowerLimit = 1.67;
+        private const double outputFluidTemperatureTolerance = 5;
+
+        private bool radiatorRotationIncreasing = true;
+
+        #endregion Limits
+
+        #region Public Properties
+        public string ComponentName => "ExternalCoolantSystem";
+
         public DateTimeOffset ReportDateTime { get; set; } = DateTimeOffset.Now;
 
         /// <summary>
-        /// basically on/off/failure(on by not working)
+        /// overall status of the system
         /// </summary>
-        public bool PumpOn { get; set; }
+        public SystemStatus Status { get; set; }
 
         /// <summary>
         /// position in degrees from neutral point.
@@ -20,51 +41,146 @@ namespace Orbit.Models
         [Range(-215, 215)]
         public int RadiatorRotation { get; set; }
 
-        private const int radiatorRotationUpperLimit = 215;
-        private const int radiatorRotationLowerLimit = -215;
-        private const int radiatorRotationTolerance = 10;
+        /// <summary>
+        /// pump for fluid circuit A
+        /// </summary>
+        public bool PumpAOn { get; set; }
 
         /// <summary>
-        /// pressure of fluid in the lines
+        /// pump for fluid circuit B
+        /// </summary>
+        public bool PumpBOn { get; set; }
+
+        /// <summary>
+        /// position of the valve that mixes 'hot' fluid returning from heat exchanger and 'cold' fluid 
+        /// returning from radiator. Acts like a shower temp valve. 0 = all 'hot', 100 = all 'cold'
+        /// </summary>
+        public int MixValvePosition { get; set; }
+
+        /// <summary>
+        /// pressure of fluid in loop A
         /// </summary>
         [Range(0, 600)]
-        public int FluidLinePressure { get; set; }
+        public int LineAPressure { get; set; }
 
-        private const int fluidLinePressureUpperLimit = 480;
-        private const int fluidLinePressureLowerLimit = 170;
-        private const int fluidLinePressureTolerance = 30;
+        /// <summary>
+        /// pressure of fluid in loop B
+        /// </summary>
+        [Range(0, 600)]
+        public int LineBPressure { get; set; }
+
+        /// <summary>
+        /// heats the external coolant if the heat offload from the station is too low to warm the -40F degree ammonia
+        /// to at least 36F to prevent the internal coolant fluid (water) from freezing
+        /// </summary>
+        public bool LineHeaterOn { get; set; }
+
+        /// <summary>
+        /// True if radiator is extended (deployed), false if retracted (not deployed)
+        /// </summary>
+        public bool RadiatorDeployed { get; set; }
 
         /// <summary>
         /// temperature of fluid returning from radiator flow control valve to internal/external heat exchanger
         /// </summary>
         [Range(-157, 121)]
-        public double TempFluidToHeatExchanger { get; set; }
-
-        private const double tempFluidToHeatExchangerUpperLimit = 18.22;
-        private const double tempFluidToHeatExchangerLowerLimit = 1.67;
-        private const double tempFluidToHeatExchangerTolerance = 5;
+        public double OutputFluidTemperature { get; set; }
 
         /// <summary>
-        /// heats the external coolant if the heat offload from the station is too low to warm the -40F degree ammonia
-        /// to at least 36F to prevent the internal coolant (water) from freezing
+        /// goal output fluid temperature
         /// </summary>
-        public bool LineHeaterOn { get; set; }
+        public double SetTemperature { get; set; }
 
-        /// <summary>
-        /// contains replenishment fluid and serves as additional reservoir for expanded fluid to go (increase of
-        /// pressure), and draw from when fluid contracts (decrease pressure)
-        /// </summary>
-        [Range(0, 100)]
-        public int TankLevel { get; set; }
+        #endregion Public Properties
 
-        private const int tankLevelUpperLimit = 95;
-        private const int tankLevelTolerance = 5;
+        #region Methods
 
-        public string ComponentName => "ExternalCoolantSystem";
+        public void ProcessData()
+        {
+            if (!PumpAOn || !PumpBOn)
+            {
+                Trouble();
+            }
+            if ((OutputFluidTemperature > outputFluidTemperatueUpperLimit) || (OutputFluidTemperature > SetTemperature))
+            {
+                // open the mixing valve and allow more 'cold' fluid in the mix
+                IncreaseFluidMix();
+            }
+            if ((OutputFluidTemperature < outputFluidTemperatureLowerLimit) || (OutputFluidTemperature < SetTemperature))
+            {
+                // close the mixing valve to keep more 'hot' fluid in the mix
+                DecreaseFluidMix();
+            }
+            if ((LineAPressure > fluidPressureUpperLimit) || (LineAPressure < fluidPressureLowerLimit))
+            {
+                // problem in the fluid line A, shut off pumpA to prevent pump damage 
+                PumpAOn = false;
+                Trouble();
+            }
+            if ((LineBPressure > fluidPressureUpperLimit) || (LineBPressure < fluidPressureLowerLimit))
+            {
+                // problem in the fluid line B, shut off pumpB to prevent pump damage
+                PumpBOn = false;
+                Trouble();
+            }
+            if (RadiatorDeployed)
+            {
+                RotateRadiator();
+            }
+        }
 
-        #region Alert generation
+        private void Trouble()
+        {
+            Status = SystemStatus.Trouble;
+        }
 
-        private IEnumerable<Alert> CheckRadiatorRotation()
+        private void IncreaseFluidMix()
+        {
+            // need formula: if valve opened 1 'degree', how much would temp change, use difference
+            // in outflow and set temp to determine amount to change valve
+            
+            if(MixValvePosition == 0)
+            {
+                // heater is on, turn it off
+                LineHeaterOn = false;
+            }
+            MixValvePosition++;
+        }
+
+        private void DecreaseFluidMix()
+        {
+            // need formula: if valve closed 1 'degree', how much would temp change, use difference
+            // in outflow and set temp to determine amount to change valve
+            
+            MixValvePosition--;
+            if (MixValvePosition == 0)
+            {
+                // heat load from station is too low to keep fluid above min temp, turn heater on
+                LineHeaterOn = true;
+            }
+        }
+
+        private void RotateRadiator()
+        {
+            if(radiatorRotationIncreasing && (RadiatorRotation < radiatorRotationUpperLimit))
+            {
+                RadiatorRotation++;
+            }
+            else if(!radiatorRotationIncreasing && (RadiatorRotation > radiatorRotationLowerLimit))
+            {
+                RadiatorRotation--;
+            }
+            else
+            {
+                radiatorRotationIncreasing = !radiatorRotationIncreasing;
+            }
+        }
+
+    #endregion Methods
+
+    #region Alert generation
+
+    private IEnumerable<Alert> CheckRadiatorRotation()
         {
             if (RadiatorRotation > radiatorRotationUpperLimit)
             {
@@ -161,5 +277,10 @@ namespace Orbit.Models
         }
 
         #endregion Alert generation
+
+        #region Equality Members
+
+
+        #endregion Equality Members
     }
 }
