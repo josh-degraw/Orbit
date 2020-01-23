9a2a9b7297196c89c56e2af6ff58d89bb696a981
Daniel Jones <dannyboy.2010@yahoo.com>
2020-01-22 17:13:09 -0700

    Added the facade and got most of it working. Getting the DB service running is still in progress.

---
+++ src/Orbit/SystemFacade.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using Orbit.Models;
+using Orbit.Data;
+
+namespace Orbit
+{
+    public class SystemFacade
+    {
+        private IServiceProvider ServiceProvider { get; }
+        private IServiceCollection serviceCollection;
+
+        //Will be replaced with Unity Binding
+        //public event PropertyChangedEventHandler? PropertyChanged;
+
+        private WasteWaterStorageTankData? _wasteWater;
+        private UrineSystemData? _urineSystemData;
+        private WaterProcessorData? _waterProcessorData;
+
+        public WasteWaterStorageTankData? WasteWater {
+            get => this._wasteWater;
+
+            set {
+
+                // This stuff here is required to tell the UI to update anything that references this property
+                this._wasteWater = value;
+                //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.WasteWater)));
+            }
+        }
+
+        public UrineSystemData? UrineTank {
+            get => this._urineSystemData;
+
+            set {
+                this._urineSystemData = value;
+                //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.UrineTank)));
+            }
+        }
+
+        public WaterProcessorData? PotableWaterTank {
+            get => this._waterProcessorData;
+
+            set {
+                this._waterProcessorData = value;
+                //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.PotableWaterTank)));
+            }
+        }
+
+        public UrineSystemData? UrineSystemStatus {
+            get => this._urineSystemData;
+
+            set {
+                this._urineSystemData = value;
+                //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.UrineSystemStatus)));
+            }
+        }
+
+        public WaterProcessorData? WaterProcessorStatus {
+            get => this._waterProcessorData;
+
+            set {
+                this._waterProcessorData = value;
+                //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.WaterProcessorStatus)));
+            }
+        }
+
+        public SystemFacade()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddTransient<OrbitDbContext>();
+            /*
+             * 
+             * InvalidOperationException: Unable to resolve service for type 
+             * 'Microsoft.EntityFrameworkCore.DbContextOptions`1[Orbit.Data.OrbitDbContext]' 
+             * while attempting to activate 'Orbit.Data.OrbitDbContext'.
+             * 
+             */ 
+            //serviceCollection.AddTransient<DbContextOptions>();
+            this.ServiceProvider = serviceCollection.BuildServiceProvider();   
+        }
+
+        public async void StartService()
+        {
+            using var scope = ServiceProvider.CreateScope();
+            var provider = scope.ServiceProvider;
+            var db = provider.GetRequiredService<OrbitDbContext>();
+            db.InsertSeedData();
+
+            var comp = provider.GetService<IMonitoredComponent<WasteWaterStorageTankData>>();
+            this.WasteWater = await comp.GetLatestReportAsync().ConfigureAwait(true);
+
+            var urineComp = provider.GetService<IMonitoredComponent<UrineSystemData>>();
+            this.UrineTank = await urineComp.GetLatestReportAsync().ConfigureAwait(true);
+
+            var potableWaterComp = provider.GetService<IMonitoredComponent<WaterProcessorData>>();
+            this.PotableWaterTank = await potableWaterComp.GetLatestReportAsync().ConfigureAwait(true);
+
+            var urineStatusComp = provider.GetService<IMonitoredComponent<UrineSystemData>>();
+            this.UrineSystemStatus = await urineStatusComp.GetLatestReportAsync().ConfigureAwait(true);
+
+            var waterProcessorComp = provider.GetService<IMonitoredComponent<WaterProcessorData>>();
+            this.WaterProcessorStatus = await waterProcessorComp.GetLatestReportAsync().ConfigureAwait(true);
+
+        }
+    }
+}
