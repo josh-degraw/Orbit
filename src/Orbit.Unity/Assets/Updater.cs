9883bb4ed3b4843c2915f75d8e4aeea36f044313
Josh <jdegraw42@gmail.com>
2020-02-12 16:05:00 -0700

    Added Modern UI Pack assets

---
+++ src/Orbit.Unity/Assets/Updater.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Michsky.UI.ModernUIPack;
+using UnityEngine;
+
+public class Updater : MonoBehaviour
+{
+    public UIManagerProgressBar pb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
