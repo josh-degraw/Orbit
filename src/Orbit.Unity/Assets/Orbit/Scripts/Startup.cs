c212d4f882cda58c7e59decb14b9edc419b340e2
Josh <jdegraw42@gmail.com>
2020-03-02 16:10:28 -0700

    Building for Android, trying to get to run on the device

--- src/Orbit.Unity/Assets/Orbit/Scripts/Startup.cs
+++ src/Orbit.Unity/Assets/Orbit/Scripts/Startup.cs
@@ -8,7 +8,7 @@ using UnityEditor;
 
 using UnityEngine;
 
-[InitializeOnLoad]
+//[InitializeOnLoad]
 public class Startup : MonoBehaviour
 {
     private static IServiceProvider ServiceProvider => OrbitServiceProvider.Instance;
