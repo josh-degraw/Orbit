1a8e5d8b93ac768a8c00c8faff38e90521566774
Candra <candraray@hotmail.com>
2020-03-04 08:36:55 -0700

    Working model on Unity 2018.4 vers with chart and gauge pack. Charts added to 2019 version

---
+++ src/Orbit_Unity2018/Assets/ImageSwap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageSwap : MonoBehaviour
+{
+    public Sprite imageOne;
+    public Sprite imageTwo;
+
+    public void Swap()
+    {
+        Image currentImage = GetComponent<Image>();
+            if(currentImage.sprite == imageOne)
+        {
+            currentImage.sprite = imageTwo;
+        }
+        else
+        {
+            currentImage.sprite = imageOne;
+        }
+    }
+}
---
+++ src/Orbit_Unity2018/Assets/Modern UI Pack/Scripts/Progress Bar/ProgressBar.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace Michsky.UI.ModernUIPack
+{
+    public class ProgressBar : MonoBehaviour
+    {
+        [Header("OBJECTS")]
+        public Transform loadingBar;
+        public Transform textPercent;
+
+        [Header("SETTINGS")]
+        public bool isOn;
+        public bool automatic;
+        public bool restart;
+        public bool invert;
+        [Range(0, 100)] public float currentPercent;
+        [Range(0, 100)] public int speed;
+
+        [Header("Colors")]
+        public Color barColor;
+
+        public Color warningColor = new Color(255, 218, 116);
+        public Color errorColor = new Color(255, 119, 107);
+
+        void Start()
+        {
+            var image = loadingBar.GetComponent<Image>();
+            if (isOn == false)
+            {
+                image.fillAmount = currentPercent / 100;
+                textPercent.GetComponent<TextMeshProUGUI>().text = ((int)currentPercent).ToString("F0") + "%";
+            }
+            if (barColor == default)
+            {
+                barColor = image.color;
+            }
+        }
+
+        void Update()
+        {
+            if (isOn == true)
+            {
+                if (automatic)
+                {
+                    if (currentPercent <= 100 && invert == false)
+                        currentPercent += speed * Time.deltaTime;
+
+                    else if (currentPercent >= 0 && invert == true)
+                        currentPercent -= speed * Time.deltaTime;
+
+                    if (currentPercent == 100 || currentPercent >= 100 && restart == true && invert == false)
+                        currentPercent = 0;
+
+                    else if (currentPercent == 0 || currentPercent <= 0 && restart == true && invert == true)
+                        currentPercent = 100;
+                }
+
+                loadingBar.GetComponent<Image>().fillAmount = currentPercent / 100;
+                textPercent.GetComponent<TextMeshProUGUI>().text = ((int)currentPercent).ToString("F0") + "%";
+                
+            }
+        }
+    }
+}
